062d2eaa0574f60b2562725d982a354133f8ef33
Paul Sprotte <sprottemail@googlemail.com>
2019-08-09 14:03:39 +0200

    added geofencer lib project

--- app/src/main/java/com/sprotte/geofencer/demo/MainActivity.kt
+++ app/src/main/java/com/sprotte/geofencer/demo/MainActivity.kt
@@ -2,11 +2,14 @@ package com.sprotte.geofencer.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sprotte.geofencer.Geofencer
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Geofencer()
     }
 }
---
+++ geofencer/src/main/java/com/sprotte/geofencer/Geofencer.kt
@@ -0,0 +1,3 @@
+package com.sprotte.geofencer
+
+class Geofencer ()
